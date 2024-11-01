a627879429af164954ea5f3ecc527f58331f403f
João Davi da Cruz Garbin <jdcruzgarbin@gmail.com>
2024-11-01 15:45:19 -0300

    mudei um pouco do JS

--- HTML/JS/script.js
+++ HTML/JS/script.js
@@ -4,18 +4,21 @@ function fecharmenu() {
   }
 
 function cadastro(nome, data_nasc, sexo, email, senha, confirmador_senha) {
-    nome = getElementById("nome")
-    data_nasc = getElementById("data_nasc")
-    sexo = getElementById("sexo")
-    email = getElementById("email")
-    senha = getElementById("senha")
-    confirmador_senha = getElementById("confirmador_senha")
+    nome = document.getElementById("nome").value;
+    data_nasc = document.getElementById("data_nasc").value;
+    sexo = document.getElementById("sexo").value;
+    email = document.getElementById("email").value;
+    senha = document.getElementById("senha").value;
+    confirmador_senha = document.getElementById("confirmador_senha").value;
 
-    alert (email, senha)
+    alert("email: " + email + "senha: " + senha);
+    window.location.href = "login.html";
 }
 
 function login(email, senha){
-    email = getElementById("email")
-    senha = getElementById("senha")
+    email = getElementById("email").value;
+    senha = getElementById("senha").value;
 
+    alert("email: " + email + "senha: " + senha);
+    window.location.href = "index.html";
 }
