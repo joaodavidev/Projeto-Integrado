4dcf1cfd11f1cddd94548ad6f3e5a3a989f0a026
João Davi da Cruz Garbin <jdcruzgarbin@gmail.com>
2024-11-01 21:27:23 -0300

    adicionei var

--- HTML/JS/script.js
+++ HTML/JS/script.js
@@ -4,20 +4,20 @@ function fecharmenu() {
   }
 
 function cadastro(nome, data_nasc, sexo, email, senha, confirmador_senha) {
-    nome = document.getElementById("nome").value;
-    data_nasc = document.getElementById("data_nasc").value;
-    sexo = document.getElementById("sexo").value;
-    email = document.getElementById("email").value;
-    senha = document.getElementById("senha").value;
-    confirmador_senha = document.getElementById("confirmador_senha").value;
+    var nome = document.getElementById("nome").value;
+    var data_nasc = document.getElementById("data_nasc").value;
+    var sexo = document.getElementById("sexo").value;
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    var confirmador_senha = document.getElementById("confirmador_senha").value;
 
     alert("email: " + email + "senha: " + senha);
     window.location.href = "login.html";
 }
 
 function login(email, senha){
-    email = getElementById("email").value;
-    senha = getElementById("senha").value;
+    var email = getElementById("email").value;
+    var senha = getElementById("senha").value;
 
     alert("email: " + email + "senha: " + senha);
     window.location.href = "index.html";
