fb45e3f8aeeae3ae77f5908d35bd6c3cbf372ae5
João Davi da Cruz Garbin <jdcruzgarbin@gmail.com>
2024-11-04 20:53:50 -0300

    coisas do SQL

--- HTML/BD/REPOSITORIOS/schema.sql
+++ HTML/BD/REPOSITORIOS/schema.sql
@@ -3,18 +3,42 @@ USE learnhub;
 
 CREATE TABLE cadastro (
     id_cadastro INT PRIMARY KEY auto_increment,
+    nome VARCHAR(20),
+    datanasc DATE,
+    sexo ENUM('selecione','masculino', 'feminino', 'outro'),
     email VARCHAR(100) UNIQUE,
     senha VARCHAR(32),
-    usuario VARCHAR(20),
-    datanasc DATE
+    confirmador_senha VARCHAR(32)
 );
 
 CREATE TABLE login (
-    id_login INT PRIMARY KEY auto_increment,
-    email VARCHAR(100) UNIQUE,
-    senha VARCHAR(32)
+    email VARCHAR(100) UNIQUE PRIMARY KEY,
+    senha VARCHAR(32),
+    FOREIGN KEY (email) REFERENCES cadastro(email),
+    FOREIGN KEY (senha) REFERENCES cadastro(senha)
 );
 
+CREATE TABLE estudante (
+    id_estudante INT PRIMARY KEY auto_increment,
+    nome, 
+    email,
+    datanasc,
+
+
+);
+
+CREATE TABLE professor (
+
+);
+
+CREATE TABLE adm (
+
+);
+
+CREATE TABLE materia (
+    id_materia INT PRIMARY KEY auto_increment,
+    id_login 
+);
 
 
 --Coisas de SELECT 
@@ -26,3 +50,4 @@ VALUES
 
 INSERT INTO login(email, senha) 
 VALUES ('teste@gmail.com', '123');
+
