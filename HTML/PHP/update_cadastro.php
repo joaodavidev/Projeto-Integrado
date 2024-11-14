1600e66f2ec2a307e8a8d87dbb43eead34597bd8
Laura Guillarducci Martins Ferreira <72210715+laducci@users.noreply.github.com>
2024-11-14 02:57:47 -0300

    CSS e PHP

--- HTML/PHP/update_cadastro.php
+++ HTML/PHP/update_cadastro.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "seu_banco_de_dados";
 
-// Cria conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
