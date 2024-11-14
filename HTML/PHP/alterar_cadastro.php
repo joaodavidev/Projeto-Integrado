a0acc2bcf09d55d4127eaa2c1635c2bb5ae87887
João Davi da Cruz Garbin <jdcruzgarbin@gmail.com>
2024-11-13 22:27:27 -0300

    SQL

--- HTML/PHP/alterar_cadastro.php
+++ HTML/PHP/alterar_cadastro.php
@@ -7,30 +7,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_start();
     $nome = $_POST['nome'];
     $data_nasc = $_POST['data_nasc'];
-    $sexo = $_POST['sexo'];
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
 
 
-   
-    $prcd = "UPDATE cadastro SET 
-                nome = ?, 
-                data_nasc = ?, 
-                sexo = ?, 
-                email = ?, 
-                senha = ? 
-            WHERE id_cadastro = ?";
+    $prcd = $conn->prepare("CALL update_cadastro(?, ?)");
+    $prcd->bind_param("ssi", $nome, $data_nasc, $id_cadastro);
 
-    $prcd = $conn->prepare($prcd);
-    $prcd->bind_param("sssssi", $nome, $data_nasc, $sexo, $email, $senha, $id_cadastro);
-
-    if ($stmt->execute()) {
+    if ($prcd->execute()) {
         echo "Cadastro atualizado com sucesso!";
     } else {
-        echo "Erro ao atualizar cadastro: " . $stmt->error;
+        echo "Erro ao atualizar cadastro: " . $prcd->error;
     }
 
-    $stmt->close();
+    $prcd->close();
 }
 
 $conn->close();
